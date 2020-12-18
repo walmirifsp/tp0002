d4af3a319f359af18e07bf4d3bc04a8fec2e3490
walmirifsp <48810953+walmirifsp@users.noreply.github.com>
2020-12-17 22:26:39 -0300

    Add files via upload

---
+++ tp002/Aluno.java
@@ -0,0 +1,51 @@
+/*
+walmir santos feitoza junior
+ */
+
+import java.util.UUID;
+
+public class Aluno {
+  private UUID uuid;
+  private int idade;
+  private String nome;
+  private String endereco;
+
+  public Aluno(String name,String address, int age) {
+    this.nome = name;
+    this.endereco = address;
+    this.idade = age;
+    this.uuid = UUID.randomUUID();
+  }
+
+  public void setUUID(UUID uuid) {
+    this.uuid = uuid;
+  }
+
+  public UUID getUUID() {
+    return this.uuid;
+  }
+
+  public void setIdade( int idade) {
+    this.idade = idade;
+  }
+
+  public int getIdade() {
+    return this.idade;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getNome() {
+    return this.nome;
+  }
+
+  public void setEndereco(String endereco) {
+    this.endereco = endereco;
+  }
+
+  public String getEndereco() {
+    return this.endereco;
+  }
+}
